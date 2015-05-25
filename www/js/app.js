dc16d71a50d97f7378fabffbb617d6dc4722a85c
kpolubok <kpolubok@gmail.com>
2015-05-26 00:54:32 +0200

    Resources for ios,android

---
+++ www/js/app.js
@@ -0,0 +1,136 @@
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+
+.constant('ApiEndpoint', {
+	url: 'http://mark-et.pl/pl/api/'
+	//url: 'http://localhost/app_dev.php/pl/api/'
+})
+
+.run(function($rootScope, $location, $ionicPlatform,$localstorage) {
+	$ionicPlatform.ready(function() {
+		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+		// for form inputs)
+		if (window.cordova && window.cordova.plugins.Keyboard) {
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+		}
+		if (window.StatusBar) {
+			// org.apache.cordova.statusbar required
+			StatusBar.styleDefault();
+		}
+		
+		$rootScope.globals = $localstorage.getObject('globals') || {};
+		if(!$rootScope.globals.currentUser) {
+			$location.path('/login');
+		} else {
+			$location.path('/app/default');
+		}
+	});
+	
+	$rootScope.$on('$locationChangeStart', function (event, next, current) {
+		$rootScope.globals = $localstorage.getObject('globals') || {};
+		if ($location.path() !== '/login' && !$rootScope.globals.currentUser) {
+			$location.path('/login');
+		}
+	});
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+	$stateProvider.state('app', {
+		url: "/app",
+		abstract: true,
+		templateUrl: "templates/menu.html",
+		controller: 'AppCtrl'
+	}).state('login', {
+		url: "/login",
+		templateUrl: "templates/login.html",
+		controller: 'LoginCtrl'
+	}).state('app.default', {
+		cache: false,
+		url: "/default",
+		views: {
+			'menuContent': {
+				templateUrl: "templates/default.html",
+				controller: 'AppCtrl'
+			}
+		}
+	}).state('app.auctions', {
+		url: "/auctions",
+		views: {
+			'menuContent': {
+				templateUrl: "templates/auctions.html",
+				controller: 'AuctionsCtrl'
+			}
+		}
+	}).state('app.auctions-finished', {
+		url: "/auctions/:t",
+		views: {
+			'menuContent': {
+				templateUrl: "templates/auctions.html",
+				controller: 'AuctionsCtrl'
+			}
+		}
+	}).state('app.auctions-continue', {
+		url: "/auctions/:t",
+		views: {
+			'menuContent': {
+				templateUrl: "templates/auctions.html",
+				controller: 'AuctionsCtrl'
+			}
+		}
+	}).state('app.auction', {
+		url: "/auction/:auctionId",
+		views: {
+			'menuContent': {
+				templateUrl: "templates/auction.html",
+				controller: 'AuctionCtrl'
+			}
+		}
+	}).state('app.offers', {
+		url: "/offers",
+		views: {
+			'menuContent': {
+				templateUrl: "templates/offers.html",
+				controller: 'OffersCtrl'
+			}
+		}
+	}).state('app.offers-continue', {
+		url: "/offers/:t",
+		views: {
+			'menuContent': {
+				templateUrl: "templates/offers.html",
+				controller: 'OffersCtrl'
+			}
+		}
+	}).state('app.offers-wybrane', {
+		url: "/offers/:t",
+		views: {
+			'menuContent': {
+				templateUrl: "templates/offers.html",
+				controller: 'OffersCtrl'
+			}
+		}
+	}).state('app.offer', {
+		url: "/offer/:offerId",
+		views: {
+			'menuContent': {
+				templateUrl: "templates/offer.html",
+				controller: 'OfferCtrl'
+			}
+		}
+	});
+	$urlRouterProvider.otherwise('/login');
+}).factory('$localstorage', ['$window', function($window) {
+	return {
+		set: function(key, value) {
+			$window.localStorage[key] = value;
+		},
+		get: function(key, defaultValue) {
+			return $window.localStorage[key] || defaultValue;
+		},
+		setObject: function(key, value) {
+			$window.localStorage[key] = JSON.stringify(value);
+		},
+		getObject: function(key) {
+			return JSON.parse($window.localStorage[key] || '{}');
+		}
+	}
+}]);
---
+++ www/js/services.js
@@ -0,0 +1,119 @@
+angular.module('starter.services', [])
+.factory('Auctions', function($http,ApiEndpoint,$rootScope,$q) {
+	var _this = this;
+	_this.auctions = null;
+	_this.data = null;
+	_this.auctions = [
+		{ name: 'Sprzedaz paszy', id: 1,f:1 },
+		{ name: 'Zakup ciagnikow', id: 2,f:1 },
+		{ name: 'Pryetarg na biurka', id: 3,f:1 },
+		{ name: 'Aukcja dobrocyznna', id: 4,f:0 },
+		{ name: 'Posyukiwania komputerow', id: 5,f:0 },
+		{ name: 'Cos tam cos tam', id: 6,f:0 }
+	  ];
+	
+	return {
+		all: function(){
+			return _this.auctions;
+		},
+		fetch: function(offers,termin,typ,zoferta){			
+			var token = $rootScope.globals.currentUser.token;
+			var q = $q.defer();
+			var p = {limit:1000};
+			
+			if(offers) {
+				p.offers = 'true'
+			}
+			
+			//1 -trwa ; 2-zakonczone			
+			if(termin) {
+				if(termin == '2') { p.archive = 'true'; }
+			} else { p.archive = 'all'; }
+			//1-otwarte;2-zamkniete
+			if(typ) {
+				if(typ == '1') { p.type = 'open'; }
+				if(typ == '2') { p.type = 'close'; }
+			}
+			if(zoferta) {
+				p.hasoffer = true;
+			}
+			
+			$http.get(
+				ApiEndpoint.url+encodeURIComponent(token)+'/auctions/0',
+				{ params: p })
+				.success(function(data) {					
+					_this.data = data;					
+					q.resolve(data);
+				})
+				.error(function(error){
+					q.reject(error);
+				});
+			return q.promise;
+		},
+		get: function(id){
+			var token = $rootScope.globals.currentUser.token;
+			var q = $q.defer();
+			$http.get(
+				ApiEndpoint.url+encodeURIComponent(token)+'/show/'+encodeURIComponent(id))
+				.success(function(data) {			
+					_this.data = data;					
+					q.resolve(data);
+				})
+				.error(function(error){
+					q.reject(error);
+				});
+			return q.promise;
+		},
+		offer: function(id){
+			var token = $rootScope.globals.currentUser.token;
+			var q = $q.defer();
+			$http.get(
+				ApiEndpoint.url+encodeURIComponent(token)+'/offer/'+encodeURIComponent(id))
+				.success(function(data) {			
+					_this.data = data;					
+					q.resolve(data);
+				})
+				.error(function(error){
+					q.reject(error);
+				});
+			return q.promise;		
+		}
+	}
+})
+
+.factory('AuthenticationService', function($rootScope,$localstorage,$http, $q, ApiEndpoint) {
+	return {
+		username: function() {
+			return $rootScope.globals.currentUser.username;
+		},
+		token: function() {
+			return $rootScope.globals.currentUser.token;
+		},
+		Login: function(username,password){
+			var q = $q.defer();
+			$http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+			$http.post(ApiEndpoint.url+'login',{ timeout: 5000,username: username, password: password }) 
+				.success(function(data) {
+					q.resolve(data);
+				})
+				.error(function(error){
+					q.reject(error);
+				});
+			return q.promise;
+		},
+		SetCredentials: function (username, token) {  
+            $rootScope.globals = {
+                currentUser: {
+                    username: username,
+                    token: token
+                },			
+            };
+            $localstorage.setObject('globals', $rootScope.globals);
+        },  
+        ClearCredentials: function () {
+            $rootScope.globals = {};
+            $localstorage.setObject('globals', null);
+        }
+	}
+})
+;
